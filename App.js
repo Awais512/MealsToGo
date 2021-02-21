a7c56928a055fe0015488fc873317c45c0e88d89
awais512 <raza.awais25@gmail.com>
2021-02-21 16:04:14 +0500

    started with restaurants feature

--- App.js
+++ App.js
@@ -1,44 +1,13 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-  Platform,
-  StatusBar,
-} from 'react-native';
-import { Searchbar } from 'react-native-paper';
 
-console.log(StatusBar.currentHeight);
+import RestaurantsScreen from './src/features/restaurants/screen/RestaurantsScreen';
 
 export default function App() {
   return (
     <>
-      <SafeAreaView style={styles.container}>
-        <View style={styles.search}>
-          <Searchbar />
-        </View>
-        <View style={styles.list}>
-          <Text>list</Text>
-        </View>
-      </SafeAreaView>
+      <RestaurantsScreen />
       <ExpoStatusBar style='auto' />
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight,
-  },
-  search: {
-    padding: 16,
-  },
-  list: {
-    flex: 1,
-    padding: 16,
-    backgroundColor: 'blue',
-  },
-});
---
+++ src/features/restaurants/components/RestaurantsInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const RestaurantsInfo = () => {
+  return (
+    <>
+      <Text>Resturant info</Text>
+    </>
+  );
+};
+
+export default RestaurantsInfo;
