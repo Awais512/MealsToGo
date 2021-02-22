337a27698f5bb7557b1e5d1c593eade94a286dbc
awais512 <raza.awais25@gmail.com>
2021-02-22 16:29:02 +0500

    Clean up theme

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
-
+import { ThemeProvider } from 'styled-components/native';
 import RestaurantsScreen from './src/features/restaurants/screen/RestaurantsScreen';
 import { theme } from './src/infrastructure/theme';
 
@@ -9,7 +8,6 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        {/* {JSON.stringify(theme.colors.ui.primary)} */}
         <RestaurantsScreen />
       </ThemeProvider>
       <ExpoStatusBar style='auto' />
