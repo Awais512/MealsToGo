88da36c23b6b91d57e71f4f12cc5ea1d8ce0e1e7
awais512 <raza.awais25@gmail.com>
2021-02-26 17:19:20 +0500

    Tab Bar Icons

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 import { ThemeProvider } from 'styled-components/native';
 import { RestaurantsScreen } from './src/features/restaurants/screen/RestaurantsScreen';
 import { theme } from './src/infrastructure/theme';
@@ -42,7 +43,28 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color, size }) => {
+                let iconName;
+
+                if (route.name === 'Restaurants') {
+                  iconName = 'md-restaurant';
+                } else if (route.name === 'Settings') {
+                  iconName = 'md-settings';
+                } else if (route.name === 'Map') {
+                  iconName = 'md-map';
+                }
+
+                // You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: 'tomato',
+              inactiveTintColor: 'gray',
+            }}
+          >
             <Tab.Screen name='Restaurants' component={RestaurantsScreen} />
             <Tab.Screen name='Map' component={Map} />
             <Tab.Screen name='Settings' component={Settings} />
