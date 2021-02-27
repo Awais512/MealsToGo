09f0e345be25ee3ac453dc06e91f7a7663bc711d
awais512 <raza.awais25@gmail.com>
2021-02-27 15:35:41 +0500

    Transform data part 2

--- src/services/restaurents/restaurents.service.js
+++ src/services/restaurents/restaurents.service.js
@@ -5,16 +5,20 @@ export const restaurantsRequest = (location = '37.7749295,-122.4194155') => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
     if (!mock) {
-      reject('Not Found');
+      reject('not found');
     }
     resolve(mock);
   });
 };
 
-restaurantsRequest()
-  .then((result) => {
-    console.log(camelize(result));
-  })
-  .catch((err) => {
-    console.log(err);
+const restaurantsTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === 'CLOSED_TEMPORARILY',
+    };
   });
+
+  return camelize(mappedResults);
+};
