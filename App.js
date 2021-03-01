24f5811661e9f6186dd29bac2cd290c61dfbaf73
awais512 <raza.awais25@gmail.com>
2021-03-01 14:11:15 +0500

    Setting up geocoding context and service

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { Ionicons } from '@expo/vector-icons';
 import { ThemeProvider } from 'styled-components/native';
 import { RestaurantsScreen } from './src/features/restaurants/screen/RestaurantsScreen';
 import { RestaurantsContextProvider } from './src/services/restaurents/restaurents.context';
+import { LocationContextProvider } from './src/services/location/location.context';
+
 import { theme } from './src/infrastructure/theme';
 import {
   useFonts as useOswald,
@@ -58,21 +60,23 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsContextProvider>
-          <NavigationContainer>
-            <Tab.Navigator
-              screenOptions={createScreenOptions}
-              tabBarOptions={{
-                activeTintColor: 'tomato',
-                inactiveTintColor: 'gray',
-              }}
-            >
-              <Tab.Screen name='Restaurants' component={RestaurantsScreen} />
-              <Tab.Screen name='Map' component={Map} />
-              <Tab.Screen name='Settings' component={Settings} />
-            </Tab.Navigator>
-          </NavigationContainer>
-        </RestaurantsContextProvider>
+        <LocationContextProvider>
+          <RestaurantsContextProvider>
+            <NavigationContainer>
+              <Tab.Navigator
+                screenOptions={createScreenOptions}
+                tabBarOptions={{
+                  activeTintColor: 'tomato',
+                  inactiveTintColor: 'gray',
+                }}
+              >
+                <Tab.Screen name='Restaurants' component={RestaurantsScreen} />
+                <Tab.Screen name='Map' component={Map} />
+                <Tab.Screen name='Settings' component={Settings} />
+              </Tab.Navigator>
+            </NavigationContainer>
+          </RestaurantsContextProvider>
+        </LocationContextProvider>
       </ThemeProvider>
 
       <ExpoStatusBar style='auto' />
