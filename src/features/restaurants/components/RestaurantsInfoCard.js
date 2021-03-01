4faedf8bdd15645b18cb8963a739a31c36b18e8d
awais512 <raza.awais25@gmail.com>
2021-03-01 16:43:16 +0500

    Key Warnings

--- src/features/restaurants/components/RestaurantsInfoCard.js
+++ src/features/restaurants/components/RestaurantsInfoCard.js
@@ -27,6 +27,7 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     ],
     address = '100 some random street',
     isOpenNow = true,
+    placeId,
     rating = 4,
     isClosedTemporarily = true,
   } = restaurant;
@@ -40,8 +41,13 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
         <Text variant='label'>{name}</Text>
         <Section>
           <Rating>
-            {ratingArray.map(() => (
-              <SvgXml xml={star} width={20} height={20} />
+            {ratingArray.map((_, i) => (
+              <SvgXml
+                key={`star-${placeId}-${i}`}
+                xml={star}
+                width={20}
+                height={20}
+              />
             ))}
           </Rating>
           <SectionEnd>
