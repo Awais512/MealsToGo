da27a4cef80a1d4f72487ed6ae7bde8186a96cd7
awais512 <raza.awais25@gmail.com>
2021-03-04 16:00:45 +0500

    Setting up stack navigation

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
 import { ThemeProvider } from 'styled-components/native';
-import { RestaurantsScreen } from './src/features/restaurants/screen/RestaurantsScreen';
 import { RestaurantsContextProvider } from './src/services/restaurents/restaurents.context';
 import { LocationContextProvider } from './src/services/location/location.context';
 
