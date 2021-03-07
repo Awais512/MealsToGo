6e350fbcdffadba35d11f0c4cd0bc47a6387fe11
awais512 <raza.awais25@gmail.com>
2021-03-07 15:45:17 +0500

    Installing firebase

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import React from 'react';
+import * as firebase from 'firebase';
 import { ThemeProvider } from 'styled-components/native';
 import { RestaurantsContextProvider } from './src/services/restaurents/restaurents.context';
 import { LocationContextProvider } from './src/services/location/location.context';
@@ -13,6 +14,17 @@ import {
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
 
 import { Navigation } from './src/infrastructure/navigation';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDJo4s1NjdhWDdAsb5S_vE2NB5jLuesoLU',
+  authDomain: 'thesocial-c43f1.firebaseapp.com',
+  projectId: 'thesocial-c43f1',
+  storageBucket: 'thesocial-c43f1.appspot.com',
+  messagingSenderId: '819059385020',
+  appId: '1:819059385020:web:b283b23277542ca93066b9',
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
