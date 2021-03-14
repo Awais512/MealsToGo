f2b0724b46ea3cab09199deee283282145a2b58b
awais512 <raza.awais25@gmail.com>
2021-03-14 14:05:02 +0500

    Account screen button

--- src/features/account/screens/AccountScreen.js
+++ src/features/account/screens/AccountScreen.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import { AccountBackground, AccountCover } from '../components/AccountStyles';
+import {
+  AccountBackground,
+  AccountContainer,
+  AccountCover,
+  AuthButton,
+} from '../components/AccountStyles';
+import { Spacer } from '../../../components/Spacer';
 
-export const AccountScreen = () => {
+export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AccountContainer>
+        <AuthButton
+          icon='lock-open-outline'
+          mode='contained'
+          onPress={() => navigation.navigate('Login')}
+        >
+          Login
+        </AuthButton>
+        <Spacer size='large'>
+          <AuthButton
+            icon='lock-open-outline'
+            mode='contained'
+            onPress={() => navigation.navigate('Register')}
+          >
+            Register
+          </AuthButton>
+        </Spacer>
+      </AccountContainer>
     </AccountBackground>
   );
 };
