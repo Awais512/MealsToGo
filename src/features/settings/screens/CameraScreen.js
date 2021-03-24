408c17fd1a8c0e4b4727aae0828753024967e1d9
awais512 <raza.awais25@gmail.com>
2021-03-24 11:51:12 +0500

    Store and Retrieve Picture using asyncstorage

--- src/features/settings/screens/CameraScreen.js
+++ src/features/settings/screens/CameraScreen.js
@@ -1,22 +1,27 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, useContext } from 'react';
 import { Camera } from 'expo-camera';
 import styled from 'styled-components/native';
 import { View, TouchableOpacity } from 'react-native';
 import { Text } from '../../../components/typography/Text';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { AuthenticationContext } from '../../../services/authentication/authentication.context';
 
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
 `;
 
-export const CameraScreen = () => {
+export const CameraScreen = ({ navigation }) => {
   const [hasPermission, setHasPermission] = useState(null);
+  const { user } = useContext(AuthenticationContext);
   const cameraRef = useRef();
 
   const snap = async () => {
     if (cameraRef) {
       const photo = await cameraRef.current.takePictureAsync();
       console.log(photo);
+      AsyncStorage.setItem(`${user.uid}-photo`, photo.uri);
+      navigation.goBack();
     }
   };
 
--- src/features/settings/screens/SettingsScreen.js
+++ src/features/settings/screens/SettingsScreen.js
@@ -1,11 +1,13 @@
-import React, { useContext } from "react";
-import { List, Avatar } from "react-native-paper";
-import { SafeArea } from "../../../components/utility/SafeAreaComponent";
-import { AuthenticationContext } from "../../../services/authentication/authentication.context";
-import styled from "styled-components/native";
-import { Text } from "../../../components/typography/Text";
-import { Spacer } from "../../../components/Spacer";
-import { TouchableOpacity } from "react-native";
+import React, { useContext, useEffect, useState } from 'react';
+import { List, Avatar } from 'react-native-paper';
+import { SafeArea } from '../../../components/utility/SafeAreaComponent';
+import { AuthenticationContext } from '../../../services/authentication/authentication.context';
+import styled from 'styled-components/native';
+import { Text } from '../../../components/typography/Text';
+import { Spacer } from '../../../components/Spacer';
+import { TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 
 const SettingsItem = styled(List.Item)`
   padding: ${(props) => props.theme.space[3]};
@@ -17,28 +19,47 @@ const AvatarContainer = styled.View`
 
 export const SettingsScreen = ({ navigation }) => {
   const { onLogout, user } = useContext(AuthenticationContext);
+  const [photo, setPhoto] = useState(null);
+
+  const getProfilePicture = async (currentUser) => {
+    const photoUri = await AsyncStorage.getItem(`${currentUser.uid}-photo`);
+    setPhoto(photoUri);
+  };
+
+  useFocusEffect(() => {
+    getProfilePicture(user);
+  }, [user]);
 
   return (
     <SafeArea>
       <AvatarContainer>
-        <TouchableOpacity onPress={() => navigation.navigate("CameraScreen")}>
-          <Avatar.Icon size={180} icon="human" backgroundColor="#2182BD" />
+        <TouchableOpacity onPress={() => navigation.navigate('CameraScreen')}>
+          {!photo && (
+            <Avatar.Icon size={180} icon='human' backgroundColor='#2182BD' />
+          )}
+          {photo && (
+            <Avatar.Image
+              size={180}
+              source={{ uri: photo }}
+              backgroundColor='#2182BD'
+            />
+          )}
         </TouchableOpacity>
-        <Spacer position="top" size="large">
-          <Text variant="label">{user.email}</Text>
+        <Spacer position='top' size='large'>
+          <Text variant='label'>{user.email}</Text>
         </Spacer>
       </AvatarContainer>
 
       <List.Section>
         <SettingsItem
-          title="Favourites"
-          description="View your favourites"
-          left={(props) => <List.Icon {...props} color="black" icon="heart" />}
-          onPress={() => navigation.navigate("Favourites")}
+          title='Favourites'
+          description='View your favourites'
+          left={(props) => <List.Icon {...props} color='black' icon='heart' />}
+          onPress={() => navigation.navigate('Favourites')}
         />
         <SettingsItem
-          title="Logout"
-          left={(props) => <List.Icon {...props} color="black" icon="door" />}
+          title='Logout'
+          left={(props) => <List.Icon {...props} color='black' icon='door' />}
           onPress={onLogout}
         />
       </List.Section>
