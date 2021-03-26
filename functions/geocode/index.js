f1ffc55e947739ce533da3721d90ae1dacc63947
awais512 <raza.awais25@gmail.com>
2021-03-26 22:52:36 +0500

    Node sdk settings

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,9 +1,27 @@
 const { locations: locationsMock } = require('./geocode.mock');
 const url = require('url');
 
-module.exports.geocodeRequest = (request, response) => {
-  const { city } = url.parse(request.url, true).query;
-  const locationMock = locationsMock[city.toLowerCase()];
+module.exports.geocodeRequest = (request, response, client) => {
+  const { city, mock } = url.parse(request.url, true).query;
+  if (mock === 'true') {
+    const locationMock = locationsMock[city.toLowerCase()];
 
-  response.json(locationMock);
+    response.json(locationMock);
+  }
+
+  client
+    .geocode({
+      params: {
+        address: city,
+        key: 'apikey',
+      },
+      timeout: 1000,
+    })
+    .then((res) => {
+      return res.json(res.data);
+    })
+    .catch((e) => {
+      return response.status(400);
+      return response.send(e.response.data.error_message);
+    });
 };
